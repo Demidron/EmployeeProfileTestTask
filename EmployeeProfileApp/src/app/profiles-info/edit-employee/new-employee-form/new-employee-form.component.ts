29f04e3893e97f9b35ab420e7f0b08d9eb957a13
Demidron <andrew.demidyuk@gmail.com>
2021-11-06 02:57:07 +0200

    Create form

--- EmployeeProfileApp/src/app/profiles-info/edit-employee/new-employee-form/new-employee-form.component.ts
+++ EmployeeProfileApp/src/app/profiles-info/edit-employee/new-employee-form/new-employee-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef,OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { jqxFormComponent } from 'jqwidgets-ng/jqxform';
+import { jqxWindowComponent } from 'jqwidgets-ng/jqxwindow';
+import { Subscription } from 'rxjs';
+import { EmployeeInfoView } from '../../models/views/employee-info-view.model';
+import { EmployeeService } from '../../services/employee.service';
+import { TypesForm } from '../enums/types-form';
 
 @Component({
   selector: 'app-new-employee-form',
@@ -7,9 +14,136 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEmployeeFormComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('windowReference') window: jqxWindowComponent;
+  @ViewChild('windowContent') windowContent:ElementRef;
+  
+  formWidget:any;
+  typeForm:TypesForm=TypesForm.CREATE;
+  private data:EmployeeInfoView=new EmployeeInfoView();
+  private subscription: Subscription;
+
+  @ViewChild('jform') myForm: jqxFormComponent;
+  constructor(private router:Router,private activateRoute: ActivatedRoute, public empService:EmployeeService) { }
 
   ngOnInit(): void {
+    this.subscription = this.activateRoute.params.subscribe(params=>{
+     this.typeForm=params[TypesForm.TYPE];
+      this.createForm();
+    });
+  }
+
+  createForm(){
+    if(this.typeForm!=TypesForm.CREATE&&this.typeForm!=TypesForm.EDIT){
+      console.log("non type");
+      return;
+    }
+    
+  
+    let columns:Array<jqwidgets.FormTemplateItem>= [
+      {
+        name:"submitButton",
+        type: 'button',
+        text: this.typeForm==TypesForm.CREATE ? 'Create' : 'Edit',
+        width: '90px',
+        columnWidth: '50%',
+        align: 'right'
+      },
+      {
+        name:"cancelButton",
+        type: 'button',
+        text: 'Cancel',
+        width: '90px',
+        columnWidth: '50%'
+      }                
+    ];
+    let template: Array<jqwidgets.FormTemplateItem> = [
+      {
+        bind: 'firstName',
+        type: 'text',
+        label: 'First name',
+        required: true,
+        labelWidth: '85px',
+        width: '250px',
+        info: 'Enter first name',
+        infoPosition: 'right'
+      },
+      {
+        bind: 'lastName',
+        type: 'text',
+        label: 'Last name',
+        required: true,
+        labelWidth: '85px',
+        width: '250px',
+        info: 'Enter last name',
+        infoPosition: 'right'
+      },
+      {
+        bind: 'city',
+        type: 'text',
+        label: 'City',
+        required: true,
+        labelWidth: '85px',
+        width: '250px'
+      },
+      {
+        bind: "genderId",
+        type: 'option',
+        label: 'Gender',
+        required: true,
+        labelWidth: '85px',
+        width: '250px',
+        component: 'jqxDropDownList',
+        options: this.empService.genders
+      },
+  
+      {
+        type: 'blank',
+        rowHeight: '10px'
+      },
+      {
+        columns: columns
+      }
+  
+    ];
+  
+    if(this.formWidget){
+      this.formWidget.destroy();
+      //restore deleted jform div
+      let widgetContainer = document.createElement('div');
+      widgetContainer.id="jform";
+      this.windowContent.nativeElement.appendChild(widgetContainer);
+    }
+
+    this.data = this.typeForm==TypesForm.CREATE ? new EmployeeInfoView() : this.empService.empInfo;
+    this.formWidget=jqwidgets.createInstance('#jform', 'jqxForm', {template:template, value:this.data});
+    let btnSub = this.formWidget.getComponentByName('submitButton');
+     btnSub.on('click',()=>{ this.onSubmit()});
+     let btnCan = this.formWidget.getComponentByName('cancelButton');
+     btnCan.on('click',()=>{this.onCancel() });
+     this.formWidget.addEventHandler('formDataChange',(event: any) => {
+      var args = event.args;
+      this.data = args.value as EmployeeInfoView;
+    });
+  }
+  onCancel(){
+    if(this.typeForm==TypesForm.EDIT){this.empService.refreshEmpInfoById(this.empService.empInfo.id)}
+    this.window.close();
+  }
+  onSubmit(){
+    if(this.typeForm==TypesForm.EDIT){
+      this.empService.updateEmp(this.data);
+     
+    }else if(this.typeForm==TypesForm.CREATE){
+      this.empService.addNewEmp(this.data);
+    }
+    this.window.close();
+  }
+  ngAfterViewInit(){
+    this.window.position({ x:  50, y:  50 });
+    this.window.onClose.subscribe(res=>{ this.router.navigate(['employees']);});
+  }
+  ngOnDestroy(){
+    this.window.destroy();
   }
 
 }
